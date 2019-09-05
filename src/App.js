d1a2701029658823f58688cbd857ef9253c7afe4
Fabian Fabro <fabrofabian@gmail.com>
2019-09-04 22:42:48 -0700

    integrated redux and proptypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-  class App extends React.Component{
-    render(){
-      return (
-        <div>
-          
-        </div>
-      );
-    }
-
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default App;
