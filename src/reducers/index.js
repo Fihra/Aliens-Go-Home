c7c40aa9815c8068398831931da1564e94ffa0b1
Fabian Fabro <fabrofabian@gmail.com>
2019-09-05 10:52:09 -0700

    working on moveObjects with redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-const initialState = {
-    message: "It's easy to integrate React and Redux, isn't it?",
-};
+import { MOVE_OBJECTS } from '../actions';
+import moveObjects from './moveObjects';
 
-const reducer = (state = initialState) => {
-    return state;
+const initialState = {
+    angle: 45,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case MOVE_OBJECTS:
+            return moveObjects(state, action);
+        default:
+            return state;
+    }   
 }
 
 export default reducer;
