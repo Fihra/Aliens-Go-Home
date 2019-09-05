bb3a54241ce1d2cb7ca7172bb5f0194988e09fe0
Fabian Fabro <fabrofabian@gmail.com>
2019-09-05 11:04:36 -0700

    mouse moves cannon

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,17 +4,18 @@ import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = (props) => {
     const viewBox = [window.innerWidth/-2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
     return (
         <svg
             id="aliens-go-home-canvas"
             preserveAspectRatio="xMaxYMax none"
+            onMouseMove={props.trackMouse}
             viewBox={viewBox}
         >
             <Sky />
             <Ground />
-            <CannonPipe rotation={45} />
+            <CannonPipe rotation={props.angle} />
             <CannonBase />
         </svg>
     );
