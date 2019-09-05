e8460a69b853ad4031d4b3788556255f92852913
Fabian Fabro <fabrofabian@gmail.com>
2019-09-05 13:24:22 -0700

    Created cannonball

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,9 @@ const initialState = {
     angle: 45,
 }
 
+//Sets up reducer that will call to action
 const reducer = (state = initialState, action) => {
+    //checks which action is being called
     switch (action.type) {
         case MOVE_OBJECTS:
             return moveObjects(state, action);
